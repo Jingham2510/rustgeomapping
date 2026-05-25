af08afc2237ee4db4d855143f57edd943ff950db
Jingham2510 <jsphingham@gmail.com>
2026-05-25 13:20:44 +0100

    change visibility of funcs

--- src/analysis/data_handler.rs
+++ src/analysis/data_handler.rs
@@ -163,19 +163,10 @@ fn str_to_force(x: &str) -> Result<[f64; 6], anyhow::Error> {
 }
 
 ///Removes the first and last character from a string
-pub fn rem_first_and_last(value: &str) -> &str {
+fn rem_first_and_last(value: &str) -> &str {
     //Turn the string to characters - then manipulate
     let mut chars = value.chars();
     chars.next();
     chars.next_back();
     chars.as_str()
-}
-
-///Turn a string delimited by "," into a vector
-pub fn str_to_vector(inp: &str) -> Vec<f64> {
-    //Create the vector delimited by ,
-    let sep: Vec<_> = inp.split(",").collect();
-
-    //Remap all the values to become f32s
-    sep.iter().flat_map(|x| x.parse()).collect()
 }
