f6a616898db6552e1e98f3a34684fb711b9bdf36
Jingham2510 <jsphingham@gmail.com>
2026-05-26 09:55:30 +0100

    update cargo.toml, update git ignore

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,30 @@
 use rustgeomapping::depth_cam::{DepthCam, Required};
 use rustgeomapping::backend::realsense::realsense_cam::RealsenseCam;
 
+use std::io::stdin;
 
 fn main(){
     let mut cam :DepthCam<RealsenseCam> = DepthCam::connect_realsense(0).expect("Failed to connect");
 
     println!("{}", cam);
 
-    
+    let mut host_resp = String::new();
+
+    stdin()
+        .read_line(&mut host_resp)
+        .expect("Failed to read line");
+
+
+
+
 
 
 
     let pnts = cam.get_pointcloud().expect("Failed to get points");
 
-    pnts.save_to_file("test");
+    
+    println!("{:?}", pnts.consume_points());
+    
 
 
 
